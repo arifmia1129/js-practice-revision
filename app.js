c481b0550d7b934db7cc80d136a82975a0fb7b09
arifvtti <arif.vtti@gmail.com>
2022-03-14 19:32:02 +0600

    nested object

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ console.log(rest);
 
 // function parameter as a destructuring object
 
-const dogObj = {
+/* const dogObj = {
     name: "dog",
     color: "brown",
     age: 7
@@ -36,4 +36,43 @@ const dog = ({ name, color, age }) => {
     console.log("Dog name, color & age :", name, color, age);
 }
 
-dog(dogObj);
+dog(dogObj); */
+
+// Nested Array 
+
+const dreamGirl = [
+    {
+        dream1: {
+            name: "bbu",
+            height: "5.4",
+            family: [{ father: "rock", mother: "shila", sister: "chinki" }],
+            age: undefined,
+            contactInfo: [
+                {
+                    facebook: {
+                        link: "https://www.facebook.com/",
+                        followers: "12545",
+                        status: "single",
+                        friendsList: [
+                            { name: "rofik" },
+                            { name: "jobbar" },
+                            { name: "salam" },
+                            { name: "borkot" },
+                            undefined,
+                        ],
+                    },
+                },
+                { instagram: "https://www.instagram.com/" },
+                { twitter: "https://twitter.com/" },
+                { github: "https://github.com/" },
+                { phone: ["01254823212", "02152457"] },
+            ],
+        },
+    },
+];
+
+
+const friends = dreamGirl[0].dream1.contactInfo[0].facebook.friendsList;
+for (const friend of friends) {
+    console.log(friend?.name);
+}
