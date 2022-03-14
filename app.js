5aa77336cc124fb5af65d9a34aef50c1e32c07dc
arifvtti <arif.vtti@gmail.com>
2022-03-14 19:09:06 +0600

    rest operator for object

--- app.js
+++ app.js
@@ -9,3 +9,16 @@ console.log(name, color, age); */
 // But this code run and output not error
 /* const { name, color, age } = dogObj || {};
 console.log(name, color, age); */
+
+
+// Rest operator for object
+
+/* const dog = {
+    name: "dog",
+    color: "brown",
+    age: 7
+}
+
+const { name, ...rest } = dog;
+console.log(rest);
+ */
