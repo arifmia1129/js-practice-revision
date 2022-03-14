eba8f4f6d9cebbf7928fb88516c71452cbef452d
arifvtti <arif.vtti@gmail.com>
2022-03-14 19:05:09 +0600

    error handleing method

---
+++ app.js
@@ -0,0 +1,11 @@
+// Error handleing method
+
+const dogObj = null;
+
+// This code run and output error
+/* let { name, color, age } = dogObj;
+console.log(name, color, age); */
+
+// But this code run and output not error
+/* const { name, color, age } = dogObj || {};
+console.log(name, color, age); */
